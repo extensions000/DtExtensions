3ec21160c4c405c1df04962c5cf446fbe3b295de
extensions000 <99815985+extensions000@users.noreply.github.com>
2022-02-16 08:38:09 -0600

    Dt files

---
+++ dt/list.js
@@ -0,0 +1,141 @@
+﻿appConfig.table = (function () {
+    var obj = {};
+    obj.configureTable = function (params) {
+        $(function () {
+            var exportOptions = { columns: [0, 1, 2], orthogonal: "export" };
+            var table = $('#datatable').DataTable({
+                responsive: true,
+                buttons: [
+                    {
+                        text: '<i"> Recargar</i>',
+                        titleAttr: 'Recargar tabla',
+                        className: 'btn btn-primary',
+                        action: function (e, dt, config) {
+                            dt.ajax.reload();
+                        }
+                    },
+                ],
+                ajax: function (data, callback, settings) {
+                    $.ajax({
+                        url: params.listUrl,
+                        type: "GET",
+                        dataType: "json",
+                        success: function (response) {
+                            callback(response);
+                        },
+                    });
+                },
+                columnDefs: [
+                    {
+                        targets: 0,
+                        data: 'campo1'
+                    },
+                    {
+                        targets: 1,
+                        data: 'campo1'
+                    },
+                    {
+                        targets: 2,
+                        className: "text-center",
+                        width: 150,
+                        render: function (data, type, row) {
+                            botones = "";
+                            if (type == "display") {
+                                botones += '<a title="Editar" class="btn btn-primary btn-sm edit-btn" data-style="zoom-in" href="Controller/Update"' + row.campo1 + '">Editar</a> | '
+                                botones += '<a title="Eliminar" class="btn btn-danger btn-sm" href="Marcas/Update"' + row.campo1 + '">Eliminar</a> ';
+                                  
+                            }
+                            return botones;
+                        }
+                    }
+                ]
+            });
+        });
+    };
+    return obj;
+}());
+
+//appConfig.Marcas = (function () {
+//    var obj = {};
+//    obj.configureTable = function (params) {
+//        $(function () {
+//            var exportOptions = { columns: [0, 1, 2], orthogonal: "export" };
+//            var table = $('#datatable').DataTable({
+//                responsive: true,
+//                buttons: [
+//                    {
+//                        text: '<i class="mdi mdi-refresh"> Recargar</i>',
+//                        titleAttr: 'Recargar tabla',
+//                        className: 'btn btn-primary',
+//                        action: function (e, dt, config) {
+//                            dt.ajax.reload();
+//                        }
+//                    },
+//                    {
+//                        extend: "collection",
+//                        text: '<i class="mdi mdi-export"> Exportar</i>',
+//                        className: 'btn btn-primary',
+//                        titleAttr: 'Exportar esta tabla',
+//                        buttons: [
+//                            {
+//                                extend: "excelHtml5",
+//                                text: '<i class="mdi mdi-file-excel"> Excel</i>',
+//                                exportOptions: exportOptions
+//                            },
+//                            {
+//                                extend: "csvHtml5",
+//                                text: '<i class="mdi mdi-file-excel"> CSV</i>',
+//                                exportOptions: exportOptions
+//                            },
+//                            {
+//                                extend: "pdfHtml5",
+//                                text: '<i class="mdi mdi-file-pdf"> PDF</i>',
+//                                exportOptions: exportOptions
+//                            }
+//                        ]
+//                    },
+//                ],
+//                ajax: function (data, callback, settings) {
+//                    $.ajax({
+//                        url: params.listUrl,
+//                        type: "GET",
+//                        dataType: "json",
+//                        success: function (response) {
+//                            callback(response);
+//                        },
+//                    });
+//                },
+//                columnDefs: [
+//                    {
+//                        targets: 0,
+//                        data: 'mar_Id'
+//                    },
+//                    {
+//                        targets: 1,
+//                        data: 'mar_Descripcion'
+//                    },
+//                    {
+//                        targets: 3,
+//                        className: "text-center",
+//                        width: 150,
+//                        render: function (data, type, row) {
+//                            botones = "";
+//                            if (type == "display") {
+//                                botones += '<a title="Editar" class="btn btn-primary btn-sm edit-btn" data-style="zoom-in" href="Marcas/Update"' + row.priod_Id + '">Editar</a> | '
+//                                botones += '<a title="Eliminar" class="btn btn-danger btn-sm" href="Marcas/Update"' + row.priod_Id + '">Eliminar</a> ';
+
+//                            }
+//                            return botones;
+//                        }
+//                    }
+//                ]
+//            });
+//        });
+//    };
+//    return obj;
+//}());
+
+
+
+
+
---
+++ dt/main.js
@@ -0,0 +1,57 @@
+﻿var appConfig = (function () {
+    var obj = {};    
+    function setupPlugins() {
+        $.extend(true, $.fn.dataTable.defaults, {
+            dom:
+                "<'row'<'col-md-8 text-left'B><'col-md-4'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+
+            order: [],
+            scrollCollapse: true,
+            paging: true,
+            stateSave: true,
+            processing: true,
+            searching: false,
+            lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "Todos"]],
+            pageLenght: 10,
+            displayLength: 10,
+            language: {
+                processing: "Procesando...",
+                lengthMenu: "Mostrar _MENU_ registros",
+                zeroRecords: "No se encontraron resultados",
+                emptyTable: "Ningún dato disponible en esta tabla",
+                info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+                infoFiltered: "(filtrado de un total de _MAX_ registros)",
+                infoPostFix: "",
+                search: "",
+                url: "",
+                infoThousands: ",",
+                loadingRecords: " ",
+                searchPlaceholder: "Buscar en la tabla...",
+                paginate: {
+                    first: "Primero",
+                    last: "Último",
+                    next: "Siguiente",
+                    previous: "Anterior"
+                },
+                aria: {
+                    sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                    sortDescending: ": Activar para ordenar la columna de manera descendente"
+                }
+            }
+        });
+    };
+    obj.init = function () {
+        setupPlugins();
+    }
+    return obj;
+}());
+
+$(function () {
+    appConfig.init();
+});
+
+
+
